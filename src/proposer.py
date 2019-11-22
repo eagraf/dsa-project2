6eea41637ddbee0fd7a396990391dcb355d92e41
eagraf <graf.ethan@gmail.com>
2019-11-22 15:25:53 -0500

    Proposer can handle multiple concurrent proposals for different slots

--- src/proposer.py
+++ src/proposer.py
@@ -1,19 +1,16 @@
 from message import Message
 from functools import reduce
+import time
+import signal
 
 class Proposer():
 
     def __init__(self, site, processes, messenger):
         self.processes = processes
         self.messenger = messenger
-        self.value = None 
+        self.proposals = list()
         self.maxPropNum = 0
-        self.proposalNumber = 0 
         self.index = list(processes).index(site)
-        self.promises = dict()                # Dictionary that tells whether a promise has been received from a specific site
-        self.accepted = False
-        self.slot = 0
-        self.resetPromises()
 
     # Find the next proposal number in the disjoint set for this site
     def nextNum(self):
@@ -22,16 +19,35 @@ class Proposer():
         return n
 
     # Phase 1 of the Synod algorithm
-    def prepare(self, value, slot):
+    def prepare(self, value, slot, counter=0):
         print("prepare")
-        self.proposalNum = self.nextNum()
-        self.maxPropNum = self.proposalNum
-        self.value = value
-        self.slot = slot
-        self.resetPromises()
+
+        # Make sure slots are initialized.
+        if slot >= len(self.proposals):
+            self.initializeSlots(slot)
+
+        proposal = dict()
+        proposal['value'] = value
+        proposal['proposalNum'] = self.nextNum()
+        proposal['accepted'] = False
+        proposal['counter'] = counter
+        proposal['promises'] = dict()
+        for process in self.processes:
+            proposal['promises'][process] = None
+
+        self.proposals[slot] = proposal
 
         # Send to all acceptors
-        self.messenger.sendAll('prepare', (self.maxPropNum,), slot)
+        self.messenger.sendAll('prepare', (proposal['proposalNum'],), slot)
+        #self.proposalTime = time.time()
+
+        # Set a 5 second timeout, retry up to 3 times.
+        #if counter < 3:
+        #    signal.signal(signal.SIGALRM, self.timeout)
+        #    signal.alarm(5)
+
+    def timeout(self, signum, frame):
+        self.prepare(self.value, self.slot, )
 
     def receive(self, message):
         if message.messageType == 'promise':
@@ -46,18 +62,20 @@ class Proposer():
             if message.contents[0] > self.maxPropNum:
                 self.maxPropNum = message.contents[0]
 
+        proposal = self.proposals[message.slot]
+
         # Add a received promise
-        self.promises[message.origin] = (message.contents[0], message.contents[1])
+        proposal['promises'][message.origin] = (message.contents[0], message.contents[1])
 
         # Check if a majority of promises have been received
         #numPromises = 0
-        numPromises = reduce((lambda a, b: a + (1 if b != None else 0)), self.promises.values(), 0)
-        allNull = reduce((lambda a, b: a and (b == None or b == (None, None))), self.promises.values(), True)  
+        numPromises = reduce((lambda a, b: a + (1 if b != None else 0)), proposal['promises'].values(), 0)
+        allNull = reduce((lambda a, b: a and (b == None or b == (None, None))), proposal['promises'].values(), True)  
 
-        print(self.promises)
+        print(proposal['promises'])
         print("numPromises, allNull " + str(numPromises) + ", " + str(allNull))
         if numPromises > 0 and not allNull:
-            actualPromises = list(filter((lambda a: a[1] != None and a[1][0] != None), list(self.promises.items())))
+            actualPromises = list(filter((lambda a: a[1] != None and a[1][0] != None), list(proposal['promises'].items())))
             maxProcess = actualPromises[0]
             for promise in actualPromises:
                 print(promise)
@@ -67,18 +85,16 @@ class Proposer():
 
 
         # If a majority of sites return promises, send accepted number and value
-        if numPromises > len(self.processes) / 2 and self.accepted == False:
-            self.accepted = True
+        if numPromises > len(self.processes) / 2 and proposal['accepted'] == False:
+            proposal['accepted'] = True
             if allNull:
-                v = self.value                            # if all promises had null values, use own value
+                v = proposal['value']                            # if all promises had null values, use own value
             else:
-                v = self.promises[maxProcess[0]][1]          # accVal from promise with largest accNum
+                v = proposal['promises'][maxProcess[0]][1]          # accVal from promise with largest accNum
 
-            self.messenger.sendAll('accept', (self.proposalNum, v), message.slot)
+            self.messenger.sendAll('accept', (proposal['proposalNum'], v), message.slot)
 
 
-    # Reset received promises to all be false.
-    def resetPromises(self):
-        self.accepted = False
-        for process in self.processes:
-            self.promises[process] = None
+    def initializeSlots(self, slot):
+        for i in range(len(self.proposals), slot+1):
+            self.proposals.append(None)
